3b3b02ff382ab534570cf6a4c88f2f355231e7fa
Anastasia <anastasia@example.com>
2020-04-10 15:35:18 +0300

    code

---
+++ lab03.h
@@ -0,0 +1,10 @@
+#ifndef LAB03_H_INCLUDED
+#define LAB03_H_INCLUDED
+#include "lab03.h"
+#include <iostream>
+
+using namespace std;
+void find_minmax(const vector<double> numbers, double& min, double& max);
+
+
+#endif // LAB03_H_INCLUDED
