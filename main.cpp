040cf34882857e7eba9cfdb7b5920a55269472f7
Anastasia <anastasia@example.com>
2020-05-08 13:58:06 +0300

    code: Получение аргументов и загрузка файла

--- main.cpp
+++ main.cpp
@@ -44,8 +44,19 @@ else
 }
 
 
-int main()
+int main(int argc,char* argv[])
 { curl_global_init(CURL_GLOBAL_ALL);
+if (argc>1)
+{
+    CURL * curl = curl_easy_init ();
+      if (curl) {
+     CURLcode res;
+ curl_easy_setopt (curl, CURLOPT_URL, argv[1]);
+ res = curl_easy_perform (curl);
+ curl_easy_cleanup (curl);
+ }
+    return 0;
+}
 
     Input data = read_input(cin,true);
 
