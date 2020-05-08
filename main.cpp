76c8ecfdb42c01abde486e6d2aa42fb454ad6bda
Anastasia <anastasia@example.com>
2020-05-08 14:03:50 +0300

    code: Обработка ошибок

--- main.cpp
+++ main.cpp
@@ -45,18 +45,26 @@ else
 
 
 int main(int argc,char* argv[])
-{ curl_global_init(CURL_GLOBAL_ALL);
-if (argc>1)
 {
-    CURL * curl = curl_easy_init ();
-      if (curl) {
-     CURLcode res;
- curl_easy_setopt (curl, CURLOPT_URL, argv[1]);
- res = curl_easy_perform (curl);
- curl_easy_cleanup (curl);
- }
-    return 0;
-}
+    curl_global_init(CURL_GLOBAL_ALL);
+    if (argc>1)
+    {
+        CURL * curl = curl_easy_init ();
+        if (curl)
+        {
+            CURLcode res;
+            curl_easy_setopt (curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform (curl);
+            res = CURLE_FAILED_INIT;
+            if (res != CURLE_OK)
+            {
+                cout << curl_easy_strerror(res);
+                exit (1);
+            }
+            curl_easy_cleanup (curl);
+        }
+        return 0;
+    }
 
     Input data = read_input(cin,true);
 
