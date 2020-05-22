dd02b5bbc7166f876bc9749e880baf086b5487da
Anastasia <anastasia@example.com>
2020-05-22 10:21:49 +0300

    code:Вывод номера версии Windows

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@
 #include <string>
 #include "lab03.h"
 #include "svg.h"
+#include <windows.h>
 
 using namespace std;
+
+DWORD WINAPI GetVersion(void);
+
 vector<double> input_numbers(size_t count)
 {
     vector<double> result(count);
@@ -34,18 +38,8 @@ vector<size_t> make_histogram(vector<double> numbers, size_t count)
 
 int main()
 {
-
-    size_t number_count;
-    cin >> number_count;
-    const auto numbers = input_numbers(number_count);
-
-    size_t bin_count;
-    cerr << "Enter column count: ";
-    cin >> bin_count;
-
-    const auto bins = make_histogram(numbers, bin_count);
-
-    show_histogram_svg(bins);
+  printf("Version is %x\n", GetVersion());
+  printf("Version is %u\n", GetVersion());
 
     return 0;
 }
